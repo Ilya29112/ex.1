81ec0e6c9c8709ebd0af62f765647614e3824fd2
Ilya Alexandrov <ilyaalexandrov19900@gmail.com>
2025-02-18 14:20:52 +0300

    Работа №1 Средний(3)

---
+++ ex.4.php
@@ -0,0 +1,17 @@
+<?php
+function getDaysInMonth($month) {
+ 
+    if ($month == 2) {
+        return 28;
+    } elseif ($month == 4 || $month == 6 || $month == 9 || $month == 11) {
+        return 30;
+    } else {
+        return 31;
+    }
+}
+
+echo "Введите порядковый номер месяца (1-12): ";
+$month = (int)trim(fgets(STDIN));
+
+$days = getDaysInMonth($month);
+echo "Количество дней в месяце: $days\n";
