9288eae465f8b2cff34d9e9179153b6207faa1b9
Ilya29112 <ilyaalexandrov19977@gmail.com>
2025-02-21 10:31:40 +0300

    Update ex.4.php

--- ex.4.php
+++ ex.4.php
@@ -1,9 +1,9 @@
 <?php
 function getDaysInMonth($month) {
  
-    if ($month == 2) {
+    if ($month === 2) {
         return 28;
-    } elseif ($month == 4 || $month == 6 || $month == 9 || $month == 11) {
+    } elseif ($month === 4 || $month === 6 || $month === 9 || $month === 11) {
         return 30;
     } else {
         return 31;
